167b5c1413e8d305fb0cc69179f83a48f13bcd13
henrique fan <fanhenrique@gmai.com>
2021-10-17 11:49:20 -0300

    update

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo cp translate.sh /usr/local/bin
+
+sudo chmod 755 /usr/local/bin/translate.sh
+
+name="translate-select-text"
+binding=$1
+action="/usr/local/bin/translate.sh"
+
+media_keys=org.gnome.settings-daemon.plugins.media-keys
+custom_kbd=org.gnome.settings-daemon.plugins.media-keys.custom-keybinding
+kbd_path=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$name/
+new_bindings=`gsettings get $media_keys custom-keybindings | sed -e"s>'\]>','$kbd_path']>"| sed -e"s>@as \[\]>['$kbd_path']>"`
+gsettings set $media_keys custom-keybindings "$new_bindings"
+gsettings set $custom_kbd:$kbd_path name $name
+gsettings set $custom_kbd:$kbd_path binding $binding
+gsettings set $custom_kbd:$kbd_path command $action
