b14570ccea98f440e76f169ec7dc5a7243acad15
Henrique Fan <fanhenrique@gmail.com>
2026-09-13 16:08:40 -0300

    dependencies

--- install.sh
+++ install.sh
@@ -7,15 +7,30 @@ TRANSLATE_FILE="$INSTALL_DIR/translate"
 CONFIG_DIR="$HOME/.config/translate-selected-text"
 CONFIG_FILE="$CONFIG_DIR/config"
 
+DEPENDENCIES=(
+    xsel
+    curl
+    jq
+    zenity
+)
+
+MISSING_DEPENDENCIES=()
+
 # Check required commands
-for cmd in xsel curl jq zenity; do
+for cmd in "${DEPENDENCIES[@]}"; do
     if ! command -v "$cmd" >/dev/null 2>&1; then
-        printf 'Error: %s is not installed.\n' "$cmd" >&2
-        exit 1
+        MISSING_DEPENDENCIES+=("$cmd")
     fi
 done
 
-INSTALL_DIR="/usr/local/bin"
+# Instsall dependencies
+if [[ ${#MISSING_DEPENDENCIES[@]} -gt 0 ]]; then
+    printf 'Installing missing dependencies: %s\n' \
+        "${MISSING_DEPENDENCIES[*]}"
+
+    sudo apt update
+    sudo apt install -y "${MISSING_DEPENDENCIES[@]}"
+fi
 
 # Install translation script
 sudo cp translate.sh "$TRANSLATE_FILE"
@@ -25,4 +40,4 @@ sudo chmod +x "$TRANSLATE_FILE"
 mkdir -p "$CONFIG_DIR"
 cp config "$CONFIG_FILE"
 
-printf 'Translation tool installed successfully.\n'
+printf '\033[32mTranslation tool installed successfully.\033[0m\n'
